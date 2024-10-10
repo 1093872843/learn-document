aaf5faacc588f6533be6ada8e2592241f451930e
conaco <1093872843@qq.com>
2024-10-10 10:29:31 +0800

    目录调整

--- js-examples-main/module-examples/top-level-await/modules/getColors.js
+++
@@ -1,5 +0,0 @@
-// fetch request
-const colors = fetch('https://mdn.github.io/js-examples/module-examples/top-level-await/data/colors.json')
-	.then(response => response.json());
-
-export default await colors;

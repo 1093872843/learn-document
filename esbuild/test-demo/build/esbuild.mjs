6b25ef5a4a083cd0bc7d1211b7bb31d830d02d7e
conaco <1093872843@qq.com>
2024-12-13 18:02:42 +0800

    esbuild: 插件1

--- esbuild/test-demo/build/esbuild.mjs
+++ esbuild/test-demo/build/esbuild.mjs
@@ -1,11 +1,25 @@
 import * as esbuild from "esbuild";
 
- await esbuild.build({
-  entryPoints: ["./entry/*.js"],
-  outdir: "web/js",
-  banner: {
-    js: '//comment',
-    css: '/*comment*/',
+import path from "node:path";
+
+let exampleOnResolvePlugin = {
+  name: "example",
+  setup(build) {
+    // 重定向所有以"images/"开头的路径到"./public/images/"
+
+    build.onResolve({ filter: /^react$/ }, (args) => {
+      args.pluginData = { customInfo: "This is custom data" };
+
+    });
+    build.onLoad({ filter: /react-dom/ }, (args) => {
+      console.log(args.path, args.pluginData);
+    });
   },
+};
+
+await esbuild.build({
+  entryPoints: ["./src/app.jsx"],
+  outdir: "web/js",
   bundle: true,
+  plugins: [exampleOnResolvePlugin],
 });
--- esbuild/test-demo/src/app.jsx
+++ esbuild/test-demo/src/app.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import * as Server from 'react-dom/server'
+import  "./foo.js"
 
 
 let Greet = () => <h1>Hello, world!</h1>
 console.log(Server.renderToString(<Greet />))
 
-
+console.log(file);
 console.error(2);
 
 

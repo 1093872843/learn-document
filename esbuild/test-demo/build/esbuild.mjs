01b88b51a80802641caa588e5a1cfe2d3eeb4f31
conaco <1093872843@qq.com>
2024-12-19 19:01:19 +0800

    esbuild: 插件onResolve

--- esbuild/test-demo/build/esbuild.mjs
+++ esbuild/test-demo/build/esbuild.mjs
@@ -1,18 +1,16 @@
 import * as esbuild from "esbuild";
 
 import path from "node:path";
-
+let n = 0;
+const cwd = process.cwd();
 let exampleOnResolvePlugin = {
   name: "example",
   setup(build) {
     // 重定向所有以"images/"开头的路径到"./public/images/"
-
     build.onResolve({ filter: /^react$/ }, (args) => {
-      args.pluginData = { customInfo: "This is custom data" };
-
-    });
-    build.onLoad({ filter: /react-dom/ }, (args) => {
-      console.log(args.path, args.pluginData);
+      return {
+        warnings: [{ text: `The path '${args.path}' is deprecated.` }],
+      };
     });
   },
 };
--- esbuild/test-demo/src/foo.js
+++ esbuild/test-demo/src/foo.js
@@ -1,3 +1,1 @@
-var API_KEY = "123";
 
-export { API_KEY };
